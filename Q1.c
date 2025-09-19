de15054971842432f64215d9c64505ee434c5666
Abdcoder-07 <k250868@nu.edu.pk>
2025-09-19 22:35:24 +0500

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    int transactions_this_hour;
+    float amount;
+    char country[50];
+    char native_country[] = "Canada";
+
+    printf("Enter Your Transaction amount: ");
+    scanf("%f", &amount);
+
+    if (amount > 5000) {
+        printf("Suspicious Alert\n");
+    } else {
+        printf("Enter Your Country For transaction: ");
+        scanf("%s", country);
+        if (strcmp(country, native_country) == 0) {
+            printf("TRANSACTION Successful\n");
+        } else {
+            printf("Suspicious\n");
+        }
+    }
+
+    printf("\nEnter the total number of transactions this hour: ");
+    scanf("%d", &transactions_this_hour);
+
+    if (transactions_this_hour >= 3) {
+        printf("Security Alert\n");
+    }
+
+    return 0;
+}
