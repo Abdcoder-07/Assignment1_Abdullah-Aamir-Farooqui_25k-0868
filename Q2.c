29ef0fb47475a05ebe8bf27c653995ee07531f38
Abdcoder-07 <k250868@nu.edu.pk>
2025-09-19 22:38:35 +0500

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int x, y;
+
+    printf("Enter the coordinates (x y): ");
+    scanf("%d %d", &x, &y);
+
+    if (x > 0 && y > 0) {
+        printf("The point (%d, %d) lies in quadrant I\n", x, y);
+    } else if (x < 0 && y > 0) {
+        printf("The point (%d, %d) lies in quadrant II\n", x, y);
+    } else if (x < 0 && y < 0) {
+        printf("The point (%d, %d) lies in quadrant III\n", x, y);
+    } else if (x > 0 && y < 0) {
+        printf("The point (%d, %d) lies in quadrant IV\n", x, y);
+    } else if (x == 0 && y == 0) {
+        printf("The point (%d, %d) is at the origin\n", x, y);
+    } else if (x == 0) {
+        printf("The point (%d, %d) is on the y-axis\n", x, y);
+    } else {
+        printf("The point (%d, %d) is on the x-axis\n", x, y);
+    }
+
+    return 0;
+}
